cf8608c19cb84591847b4be1c969bd0c06231885
Bertrand Teguia T <49428449+T3gu1a@users.noreply.github.com>
2023-03-21 16:48:40 +0100

    Controlling the starting order in Ansatz

--- NLDE/AnsatzDalg/arithmeticDeltak/src/arithmeticDeltak.mm
+++ NLDE/AnsatzDalg/arithmeticDeltak/src/arithmeticDeltak.mm
@@ -3,16 +3,18 @@ arithmeticDeltak:=proc(L::list(`=`),
 		       V::list(anyfunc(name)),
 		       z::name=ratpoly,
 		       {degreeDE::posint:=2,
-		       deorder::posint:=10},
+		       startingorder::posint:=1,
+		       maxdeorder::posint:=1},
 		       $)::`=`;
-		local t::name:=op(1,V[1]), start::posint, DEs::list(`=`), 
+		local t::name:=op(1,V[1]), start::posint, Ords::list(posint), DEs::list(`=`), 
 		      j::posint, Sys::list, subvars::list, SubL::list, subV::list;
 		option `Copyright (c) 2022 Bertrand Teguia T.`;
 		description "Ansatz method for the arithmetic of D-algebraic functions ";
 		if numelems(L)=1 then
 			return L
 		end if;
-		start:=min(map(r->PDEtools:-difforder(r,t),L));
+		Ords:=map(r->PDEtools:-difforder(r,t),L);
+		start:=max(min(Ords),startingorder);
 		DEs:=map(r->lhs(r) - rhs(r)=0,L);
 		Sys:=mergesystem(DEs,V);
 		subvars:=map(r->r=r(t),Sys[3]);
@@ -20,5 +22,6 @@ arithmeticDeltak:=proc(L::list(`=`),
 		SubL:=[seq(diff(Sys[3][j],t)=Sys[1][j],j=1..numelems(Sys[1]))];
 		subV:=[seq(op(0,V[j])=Sys[2][j],j=1..numelems(V))];
 		return DegreekDE(subs(subV,rhs(z)),lhs(z)(t),SubL,
-			maxdeorder=deorder,':-degreeDE'=degreeDE,startfromord=start)
+			':-maxdeorder'=max(add(Ords),maxdeorder),
+			':-degreeDE'=degreeDE,startfromord=start)
 	end proc:
--- NLDE/AnsatzDalg/unaryDeltak/src/unaryDeltak.mm
+++ NLDE/AnsatzDalg/unaryDeltak/src/unaryDeltak.mm
@@ -3,17 +3,20 @@ unaryDeltak:= proc(DE::`=`,
 		    y::anyfunc(name),
 		    z::name=algebraic,
 		    {degreeDE::posint:=2,
-		    deorder::posint:=10},
+		    maxdeorder::posint:=10},
 		    $)::`=`;
-		local t::name:=op(y),start::posint,Sys::list,x::nothing,var::name,subvars::list,SubL::list,j::posint;
+		local t::name:=op(y),start::posint,Sys::list,Ords::list(posint),
+		      x::nothing,var::name,subvars::list,SubL::list,j::posint;
 		option `Copyright (c) 2022 Bertrand Teguia T.`;
-		start:=PDEtools:-difforder(DE,t);
+		Ords:=map(r->PDEtools:-difforder(r,t),L);
+		start:=max(min(Ords),startingorder);
 		Sys:=buildsystem(lhs(DE) - rhs(DE)=0,y,x);
 		var:=op(0,y);
 		subvars:=map(r->r=r(t),Sys[2]);
 		Sys:=subs(subvars,Sys);
 		SubL:=[seq(diff(Sys[2][j],t)=Sys[1][j],j=1..numelems(Sys[1]))];
-		DegreekDE(subs(var=Sys[2][1],normal(rhs(z))),lhs(z)(t),SubL,maxdeorder=deorder,
-			  ':-degreeDE'=degreeDE,startfromord=start)
+		DegreekDE(subs(var=Sys[2][1],normal(rhs(z))),lhs(z)(t),SubL,
+			':-maxdeorder'=max(add(Ords),maxdeorder),
+			':-degreeDE'=degreeDE,startfromord=start)
 	end proc:
 
