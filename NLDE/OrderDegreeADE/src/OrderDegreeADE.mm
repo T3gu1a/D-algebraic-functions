e00b6411fe6f72ed7cd318bf5b1bdfc01b1e01f3
Bertrand Teguia T <49428449+T3gu1a@users.noreply.github.com>
2023-03-22 18:06:54 +0100

    lhs and rhs in OrderDegreeADE

--- NLDE/OrderDegreeADE/src/OrderDegreeADE.mm
+++ NLDE/OrderDegreeADE/src/OrderDegreeADE.mm
@@ -6,8 +6,9 @@ OrderDegreeADE:= proc(ADE::`=`,
 		      P::polynom, L::list;
 		option `Copyright (c) 2022 Bertrand Teguia T.`;
 		description "Returns [order, degree] of the input ADE";
-		r:=PDEtools:-difforder(ADE,x);
-		P:=subs([seq(diff(y,[x$j])=X[j],j=0..r)],lhs(ADE));
+		P:=lhs(ADE)-rhs(ADE);
+		r:=PDEtools:-difforder(P,x);
+		P:=subs([seq(diff(y,[x$j])=X[j],j=0..r)],P);
 		#convert P to the list of its monomials
 		#to avoid cancellation after the substitution (x=1)
 		L:=subs(x=1,convert(P,list));
