31463b3e8d5efd2cca0533ee6fe895786c6ead79
Bertrand Teguia T <49428449+T3gu1a@users.noreply.github.com>
2023-03-24 11:45:09 +0100

    Update OrderDegreeADE.mm
    Simply consider the polynomials in the dependent variables and its derivatives, and ignore the independent variable.

--- NLDE/OrderDegreeADE/src/OrderDegreeADE.mm
+++ NLDE/OrderDegreeADE/src/OrderDegreeADE.mm
@@ -3,15 +3,12 @@ OrderDegreeADE:= proc(ADE::`=`,
 		        y::anyfunc(name),
 		       $)::list(nonnegint,nonnegint);
 		local x::name:=op(y),X::name:=op(0,y),r::nonnegint,d::nonnegint,j::posint,
-		      P::polynom, L::list;
+		      P::polynom;
 		option `Copyright (c) 2022 Bertrand Teguia T.`;
 		description "Returns [order, degree] of the input ADE";
 		P:=lhs(ADE)-rhs(ADE);
 		r:=PDEtools:-difforder(P,x);
 		P:=subs([seq(diff(y,[x$j])=X[j],j=0..r)],P);
-		#convert P to the list of its monomials
-		#to avoid cancellation after the substitution (x=1)
-		L:=subs(x=1,convert(P+x,list));
-		d:=max(map(degree,L));
+		d:=degree(P,[seq(X[j],j=0..r)]);
 		return [r,d]
 	end proc:
