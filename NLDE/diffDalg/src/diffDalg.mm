3ac9555062de51a80044d75813a9096d4925357c
Bertrand Teguia T <49428449+T3gu1a@users.noreply.github.com>
2023-04-19 11:16:30 +0200

    Update diffDalg
    Covering the case where elimination is not needed

--- NLDE/diffDalg/src/diffDalg.mm
+++ NLDE/diffDalg/src/diffDalg.mm
@@ -4,7 +4,7 @@ diffDalg :=proc(DE::`=`,
 		 n::posint:=1,
 		{ordering::identical(plex,lexdeg):=plex},
 		$)::`=`;
-		local t::name:=op(y),var::name:=op(0,y),
+		local t::name:=op(y),var::name:=op(0,y),C::nothing,
 		      d::nonnegint,j::nonnegint,p,q,R,V;
 		option `Copyright (c) 2023 Bertrand Teguia T.`;
 		description "Compute a differential equation for                                   "
@@ -13,11 +13,15 @@ diffDalg :=proc(DE::`=`,
 			    "INPUT: - a differential equation                                      "
 			    "       - its dependent variable, say f(t)                             "
 			    "OUPUT: a differential equation satisfied by diff(f(t),t)              ";
-		if n=1 then	    
+		if n=1 then	
 			p:=lhs(DE)-rhs(DE);
 			d:=PDEtools:-difforder(p,t);
 			q:=diff(p,t);
 			p:=subs([seq(diff(var(t),[t$j])=var[j],j=0..d)],p);
+			#no need for elimination
+			if not has(p, var[0]) then
+				return subs([seq(var[j]=diff(var(t),[t$(j-1)]),j=1..d)],p)=0
+			end if;
 			q:=subs([seq(diff(var(t),[t$j])=var[j],j=0..d+1)],q);
 			R:=resultant(p,q,var[0]);
 			if type(R,`+`) then
