e28740a8f4b85c3e8484022fb1bfbb531a08c344
Bertrand Teguia T. <49428449+T3gu1a@users.noreply.github.com>
2024-09-30 18:03:25 +0100

    Update OrderDegreeRec.mm

--- NLDE/Shift-algebraic-sequences/OrderDegreeRec/src/OrderDegreeRec.mm
+++ NLDE/Shift-algebraic-sequences/OrderDegreeRec/src/OrderDegreeRec.mm
@@ -8,8 +8,8 @@ OrderDegreeRec:= proc(RE::`=`,
 		description "Returns [order, degree] of the input recurrence equation";
 		P:=numer(normal(lhs(RE) - rhs(RE)));
 		(r,r0):=DalgSeq:-REorders(P=0,X(n));
-		r:=r-r0;
+		r:=r;
 		P:=subs([seq(X(n+j)=X[j],j=r0..r)],P);
 		d:=degree(P,[seq(X[j],j=r0..r)]);
-		return [r,d]
+		return [r-r0,d]
 	end proc:
